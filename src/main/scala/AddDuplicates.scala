fee9eb57e6f908abd6194f67f812810e0ca86615
yamini-ban <yaminibansal7@gmail.com>
2020-02-05 09:55:16 +0530

    palindrome and add duplicates solution added

--- src/main/scala/AddDuplicates.scala
+++ src/main/scala/AddDuplicates.scala
@@ -2,6 +2,15 @@ class CustomException(message: String) extends Exception(message)
 
 class AddDuplicates {
   def duplicate(list: List[Int]): List[Int] = {
-
+    def innerDuplicate(list: List[Int], resultantList: List[Int]): List[Int] = {
+      list match {
+        case Nil => resultantList
+        case first :: rest => innerDuplicate(rest, resultantList ::: List(first, first))
+      }
+    }
+    if (list.length == 0) {
+      throw new CustomException("List is empty...")
+    }
+    innerDuplicate(list, List.empty[Int])
   }
 }
--- src/main/scala/Palindrome.scala
+++ src/main/scala/Palindrome.scala
@@ -2,6 +2,16 @@ class CustomException(message: String) extends Exception(message)
 
 class Palindrome {
   def isPalindrome(list: List[Int]): Boolean = {
-
+    def innerIsPalindrome(list: List[Int], lastElement: Int): Boolean = {
+      list match {
+        case first :: Nil => true
+        case first :: _ :: last :: Nil if first != last => false
+        case _ :: intermediate :: _ :: Nil => innerIsPalindrome(intermediate)
+      }
+    }
+    if (list.isEmpty) {
+      throw new CustomException("List is empty...")
+    }
+    innerIsPalindrome(list, list.head)
   }
 }
